fac4534f30293f4de39671b9408d5a5d329ed6af
Ayomikun <ayoadeosun10@gmail.com>
2025-03-01 09:48:31 +0100

    chore: adjusted llm prompt

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ async def general_question(request: UserQuestion):
         Any other questions regarding any other thing should be ignored.
         
         User Question: {request.question}
+        Keep responses between 160 and 210 words
         """
         chat_completion = groq_client.chat.completions.create(
             model="llama-3.3-70b-versatile",
